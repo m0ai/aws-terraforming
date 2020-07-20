29f6d5373b9cf0adb4555c8e9efb7e9ae1b2bd29
m0ai <frozenmoai@gmail.com>
2020-07-21 00:59:38 +0900

    Add a missed curly brackets on backend definition

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,16 +7,15 @@ terraform {
       name = "aws-terraforming"
     }
   }
+}
 
 
 module "lightsail" {
   source = "./modules/lightsail"
   public_key_path  = "../docs/keys/moai-pem.pub"
   name = "moai"
-
   bundle_id = "nano_2.0"
   blueprint_id = "ubuntu_18_04"
-
   availability_zone = "ap-northeast-2a"
   tags = {
     Terraform = "true"
