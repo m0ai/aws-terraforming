b48bc562bbf8e5eadabe877850fe6d8b9c0f240a
m0ai <frozenmoai@gmail.com>
2020-07-21 00:09:24 +0900

    Add a public keys as docs/keys/moai-pem.pub

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 
 module "lightsail" {
   source = "./modules/lightsail"
-  public_key_path  = ""
+  public_key_path  = "../docs/keys/moai-pem.pub"
   name = "moai"
   availability_zone = "ap-northeast-2"
   tags = {
